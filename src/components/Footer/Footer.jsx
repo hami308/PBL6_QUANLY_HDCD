7d31c44db47c09de2ab1f3cf1446ef43efdf780e
tramy984 <nguyenthitramy9804@gmail.com>
2025-10-03 16:22:24 +0700

    Add component Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import "./Footer.css";
+const Footer = () => {
+  return (
+    <footer>
+  <div className="footer-container">
+    <div className="footer-column">
+      <p><strong>Phòng Tổ chức - Hành chính</strong></p>
+      <p>ĐC: Phòng S01.04</p>
+      <p>ĐT: 0236.3842308</p>
+      <p><a href="https://dut.udn.vn/Phong/Tochuc" target="_blank">https://dut.udn.vn/Phong/Tochuc</a></p>
+    </div>
+
+    <div className="footer-column">
+      <p><strong>Phòng Khoa học Công nghệ & Hợp tác quốc tế</strong></p>
+      <p>ĐC: Phòng S06.02</p>
+      <p>ĐT: 0236.841292</p>
+      <p><a href="https://dut.udn.vn/Phong/KhoaHoc" target="_blank">https://dut.udn.vn/Phong/KhoaHoc</a></p>
+    </div>
+
+    <div className="footer-column">
+      <p><strong>Phòng Công tác sinh viên</strong></p>
+      <p>ĐC: Phòng S06.08</p>
+      <p>ĐT: 0236.3732698</p>
+      <p><a href="https://dut.udn.vn/Phong/Sinhvien" target="_blank">https://dut.udn.vn/Phong/Sinhvien</a></p>
+    </div>
+
+    <div className="footer-column">
+      <p><strong>Phòng Cơ sở Vật chất</strong></p>
+      <p>ĐC: Phòng S03.02, S03.05</p>
+      <p>ĐT: 0236.3740719</p>
+      <p><a href="https://dut.udn.vn/Phong/VatChat" target="_blank">https://dut.udn.vn/Phong/VatChat</a></p>
+    </div>
+
+    <div className="footer-column">
+      <p><strong>Phòng Đào tạo</strong></p>
+      <p>ĐC: Phòng S05.07, S05.10</p>
+      <p>ĐT: 0236.3842145</p>
+      <p><a href="https://dut.udn.vn/Phong/DaoTao" target="_blank">https://dut.udn.vn/Phong/DaoTao</a></p>
+    </div>
+
+    <div className="footer-column">
+      <p><strong>Phòng Quản lý Chất lượng và Pháp chế</strong></p>
+      <p>ĐC: Phòng S05.03</p>
+      <p>ĐT: 0236.3737931</p>
+      <p><a href="https://dut.udn.vn/Phong/QualityAssurance" target="_blank">https://dut.udn.vn/Phong/QualityAssurance</a></p>
+    </div>
+
+    <div className="footer-column">
+      <p><strong>Phòng Kế hoạch - Tài chính</strong></p>
+      <p>ĐC: Phòng S03.11</p>
+      <p>ĐT: 0236.3841660</p>
+      <p><a href="https://dut.udn.vn/Phong/TaiChinh" target="_blank">https://dut.udn.vn/Phong/TaiChinh</a></p>
+    </div>
+  </div>
+</footer>
+
+  );
+};
+
+export default Footer;
