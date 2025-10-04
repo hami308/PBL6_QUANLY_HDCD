a11ea9268fc6f0921a2e0302e0a08d21a3fff6d7
tramy984 <nguyenthitramy9804@gmail.com>
2025-10-04 22:18:53 +0700

    Add StudentInfor_Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage/HomePage";
 import { LoginProvider } from "./components/Login/LoginContext";
 import PVCD_Record from "./Pages/PVCD_Record/PVCD_Record";
+import StudentInfor_Page from "./Pages/StudentInfor_Page/StudentInfor_Page";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<HomePage />} />      
         <Route path="/home-student" element={<HomePage />} />
         <Route path="/pvcd-record" element={<PVCD_Record />} />
+        <Route path="/student-infor" element={<StudentInfor_Page />} />
       </Routes>
     </LoginProvider>
   );  
---
+++ src/Pages/StudentInfor_Page/StudentInfor_Page.jsx
@@ -0,0 +1,13 @@
+import Header from '../../components/Header/Header.jsx';
+import Menu_Student from '../../components/Menu/Menu_Student.jsx';
+function StudentInfor_Page() {
+  return (
+    <>
+        <Header />
+        <Menu_Student />
+        
+    </>
+    
+  );
+}
+export default StudentInfor_Page;
