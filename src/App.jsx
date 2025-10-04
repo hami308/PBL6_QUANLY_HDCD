79ec16356b416419b9d00d72ee5e18c9d4aa34f8
nguyenhamy2000.td@gmail.com <nguyenhamy2000.td@gmail.com>
2025-10-04 22:06:50 +0700

    Update App.jsx with Activity component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import CreateAccount_Page from "./pages/CreateAccount_Page/CreateAccount_Page";
+import CreateAccount_Page from "./Pages/CreateAccount_Page/CreateAccount_Page";
 import CreateAccount from "./components/CreateAccountBox/CreateAccountBox";
 function App() {
   return <CreateAccount_Page />;
