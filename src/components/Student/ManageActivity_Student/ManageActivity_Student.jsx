db9bdb7d2aeef64715a244590229c301f56f81d0
tramy984 <nguyenthitramy9804@gmail.com>
2025-10-06 22:54:32 +0700

    Update component ManageActivity_Student

--- src/components/Student/ManageActivity_Student/ManageActivity_Student.jsx
+++ src/components/Student/ManageActivity_Student/ManageActivity_Student.jsx
@@ -1,44 +1,49 @@
-import activity from "../../../assets/images/activity.jpg";
+
 import "./ManageActivity_Student.css";
-function ManageActivity_Student({ status = "Đã được duyệt" }){
 
-    return(
-        <div className="event-card">
-            <img src={activity} alt="Event" className="event-image" />
+function ManageActivity_Student({ name_activity, org, date, location, status, img }) {
+  return (
+    <div className="event-card">
+      <img src={img} alt="Event" className="event-image" />
 
-            <div className="event-info">
-                <div className="event-header">
-                    <h3 className="event-title">Hiến máu nhân đạo</h3>
-                </div>
-                <p className="event-club">Câu lạc bộ Công tác xã hội</p>
-                <p className="event-detail">
-                <strong>Thời gian:</strong> 15/9/2025
-                </p>
-                <p className="event-detail">
-                <strong>Địa điểm:</strong> Khu B ĐHBK
-                </p>
-            </div>
-
-            <div className="event-status-wrapper">
-                {status === "Đã tham gia" ? (
-                <>
-                    <span className="event-status joined">Đã tham gia</span>
-                    <p className="event-point">Điểm: 5 điểm</p>
-                </>
-                ) : status === "Đã được duyệt" ? (
-                <span className="event-status approved">Đã được duyệt</span>
-                ) : (
-                <span className="event-status registered">Đã đăng ký</span>
-                )}
-            </div>
-
-            <div className="event-actions">
-                {status === "Đã tham gia" && (
-                <a href="/event-rate" className="event-link">Đánh giá</a>
-                )}
-                <a href="/activity-details" className="event-link">Chi tiết</a>
-            </div>
+      <div className="event-info">
+        <div className="event-header">
+          <h3 className="event-title">{name_activity}</h3>
         </div>
-    );
+        <p className="event-club">{org}</p>
+        <p className="event-detail">
+          <strong>Thời gian:</strong> {date}
+        </p>
+        <p className="event-detail">
+          <strong>Địa điểm:</strong> {location}
+        </p>
+      </div>
+
+      <div className="event-status-wrapper">
+        {status === "Đã tham gia" ? (
+          <>
+            <span className="event-status joined">Đã tham gia</span>
+            <p className="event-point">Điểm: 5 điểm</p>
+          </>
+        ) : status === "Đã được duyệt" ? (
+          <span className="event-status approved">Đã được duyệt</span>
+        ) : (
+          <span className="event-status registered">Đã đăng ký</span>
+        )}
+      </div>
+
+      <div className="event-actions">
+        {status === "Đã tham gia" && (
+          <a href="/event-rate" className="event-link">
+            Đánh giá
+          </a>
+        )}
+        <a href="/activity-details" className="event-link">
+          Chi tiết
+        </a>
+      </div>
+    </div>
+  );
 }
+
 export default ManageActivity_Student;
