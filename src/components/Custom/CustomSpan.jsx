2e131052ce33c39248aee11b110f4dbe5d42c4a1
tramy984 <nguyenthitramy9804@gmail.com>
2025-10-10 21:29:58 +0700

    add component CustomSpan.jsx

---
+++ src/components/Custom/CustomSpan.jsx
@@ -0,0 +1,16 @@
+
+ function CustomSpan({ value, ismodify = true, className}) {
+    return (
+      <span
+        className={className}
+        contentEditable={!ismodify} 
+        suppressContentEditableWarning={true}
+        style={{
+            border: !ismodify ? "1px  #ccc" : "none", // viền để phân biệt
+        }}
+      >
+        {value}
+      </span>
+    );
+  }
+export default CustomSpan;
