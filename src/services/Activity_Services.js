b3e816259d1a9adbbd3433b80efd7e23d93d696b
tramy984 <nguyenthitramy9804@gmail.com>
2025-10-24 21:22:30 +0700

    add api activty_list

---
+++ src/services/Activity_Services.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const API_URL = "https://pbl6-backend.vercel.app/api"; 
+
+export async function get_all_activities() {
+  try {
+    const response = await axios.get(`${API_URL}/activities`);
+    return {
+      success: true,
+      data: response.data,
+    };
+  } catch (error) {
+    console.error("Get activities error:", error);
+    return {
+      success: false,
+      message:
+        error.response?.data?.message ||
+        "Lỗi kết nối đến server, vui lòng thử lại sau.",
+    };
+  }
+}
