ab3d150a9481a8f80340fb6a2b39a948f0ef5796
tramy984 <nguyenthitramy9804@gmail.com>
2025-10-29 22:36:53 +0700

    update UI dark mode HomePage

---
+++ src/services/Org_Service.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const API_URL = "https://pbl6-backend.vercel.app/api"; 
+
+export async function get_all_org() {
+  try {
+    const response = await axios.get(`${API_URL}/org-units`);
+    return {
+      success: true,
+      data: response.data,
+    };
+  } catch (error) {
+    console.error("Get activities error:", error);
+    return {
+      success: false,
+      message:
+        error.response?.data?.message ||
+        "Lỗi kết nối đến server, vui lòng thử lại sau.",
+    };
+  }
+}
+
