32240b6173312480a74d4b5e2529e0439683b9b1
tramy984 <nguyenthitramy9804@gmail.com>
2025-10-30 21:26:20 +0700

    add API ManageActivity_Student

--- src/services/Activity_Services.js
+++ src/services/Activity_Services.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://pbl6-backend-iy5q.onrender.com/api"; 
+const API_URL = "https://pbl6-backend.vercel.app/api"; 
 
 export async function get_all_activities() {
   try {
@@ -20,15 +20,39 @@ export async function get_all_activities() {
   }
 }
 
-export async function get_activity_by_id(id) {
+export async function get_details_activity_by_id(id) {
   try {
     const response = await axios.get(`${API_URL}/activities/${id}`);
+    console.log(response);
     return {
       success: true,
       data: response.data,
     };
+    
   } catch (error) {
     console.error(`Get activity ${id} error:`, error);
+    return {
+      success: false,
+      message:
+        error.response?.data?.message ||
+        "Không thể lấy thông tin hoạt động, vui lòng thử lại sau.",
+    };
+  }
+}
+export async function get_activities_by_idstudent(idstudent) {
+  try {
+    const token = sessionStorage.getItem("token");
+     const response = await axios.get(`${API_URL}/activities/student/${idstudent}`, {
+        headers: { Authorization: `Bearer ${token}` },
+    });
+    console.log(response);
+    return {
+      success: true,
+      data: response.data,
+    };
+    
+  } catch (error) {
+    console.error(`Get activity ${idstudent} error:`, error);
     return {
       success: false,
       message:
