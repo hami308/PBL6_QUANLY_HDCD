37793339a09ece6643ee03a98bae1c25cff60c4d
nguyenhamy2000.td@gmail.com <nguyenhamy2000.td@gmail.com>
2025-11-09 16:37:58 +0700

    Change the List_Activity_Create_QR link to the correct one

--- src/Pages/Create_QR_Page/Create_QR_Page.jsx
+++ src/Pages/Create_QR_Page/Create_QR_Page.jsx
@@ -1,19 +1,19 @@
 import Header from "../../components/Header/Header";
 import Menu_org from "../../components/Menu/Menu_student";
-import List_Activity_Create_QR from "../../components/Org/Create_QR_Attendance/List_Activity_create-qr";
+import List_Activity_Create_QR from "../../components/Org/Create_QR_Attendance/List_Activity_Create-QR";
 import React from "react";
 import "./Create_QR_Page.css";
 function Create_QR_Page() {
-    return (
-        <>
-            <Header />
-            <Menu_org />
-            <div className="background-create-qr"></div>
-            <div className="cross-bar">
-                <p>Danh sách các hoạt động đang tổ chức</p>
-            </div>
-            <List_Activity_Create_QR />
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <Menu_org />
+      <div className="background-create-qr"></div>
+      <div className="cross-bar">
+        <p>Danh sách các hoạt động đang tổ chức</p>
+      </div>
+      <List_Activity_Create_QR />
+    </>
+  );
 }
 export default Create_QR_Page;
