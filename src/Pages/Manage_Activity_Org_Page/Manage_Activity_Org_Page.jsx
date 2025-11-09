02102f7cf01e9c6e7829ec63684672ccfe1b0066
nguyenhamy2000.td@gmail.com <nguyenhamy2000.td@gmail.com>
2025-11-09 16:44:08 +0700

    Change the link to correct upper and lower case

--- src/Pages/Manage_Activity_Org_Page/Manage_Activity_Org_Page.jsx
+++ src/Pages/Manage_Activity_Org_Page/Manage_Activity_Org_Page.jsx
@@ -1,7 +1,7 @@
-import Header from "../../Components/Header/Header";
-import Menu_org from "../../Components/Menu/Menu_student";
+import Header from "../../components/Header/Header";
+import Menu_org from "../../components/Menu/Menu_student";
 import Filter_Activity from "../../components/Activity/Filter_activity";
-import Footer from "../../Components/Footer/Footer";
+import Footer from "../../components/Footer/Footer";
 import "./Manage_Activity_Org_Page.css";
 import List_Activity_org_component from "../../components/Org/Manage_Activity_Org/List_Activity_org_component";
 function Manage_Activity_Org_Page() {
@@ -9,8 +9,7 @@ function Manage_Activity_Org_Page() {
     <div className="manage-activity-org-page">
       <Header />
       <Menu_org />
-      <div className="background-image-manage-activity-org">
-      </div>
+      <div className="background-image-manage-activity-org"></div>
       <div className="cross-bar">
         <p>Danh sách các hoạt động</p>
       </div>
